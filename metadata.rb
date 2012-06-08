e7acd8e6a10bc532072d5f34951d5738bf692a49
Greg Symons <gsymons@gsconsulting.biz>
2012-06-08 08:59:50 -0500

    Update version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs lvm2 package"
-version           "0.7.1"
+version           "0.8.0"
 
 recipe "lvm", "Installs lvm2 package"
 
