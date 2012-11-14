4759d200098d51be59c7ff9d0fdf31870eeba8c7
jtimberman <joshua@opscode.com>
2012-11-13 23:08:14 -0700

    add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name              "lvm"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs lvm2 package"
-version           "0.8.2"
+version           "0.8.3"
 
 recipe "lvm", "Installs lvm2 package"
 
