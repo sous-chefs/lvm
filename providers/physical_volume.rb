4df5e78260cc456a865eefb6a58943712b67c89a
jtimberman <joshua@opscode.com>
2013-04-22 14:58:03 -0600

    [COOK-2733] - fix invalid only_if

--- providers/physical_volume.rb
+++ providers/physical_volume.rb
@@ -9,6 +9,6 @@ action :create do
         block do
             @lvm.raw "pvcreate #{new_resource.name}"
         end
-        only_if @lvm.physical_volumes[new_resource.name].nil?
+        only_if { @lvm.physical_volumes[new_resource.name].nil? }
     end
 end
