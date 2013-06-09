468c1d89ec651e376b91d2248a1266d2e807e409
jtimberman <joshua@opscode.com>
2013-06-09 15:45:59 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs lvm2 package"
-version           "0.8.10"
+version           "0.8.11"
 
 recipe "lvm", "Installs lvm2 package"
 
