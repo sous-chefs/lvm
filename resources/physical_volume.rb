c1ce13f2423eef151483cf39ded6b18fa41422dd
Seth Vargo <sethvargo@gmail.com>
2013-07-16 17:03:00 -0400

    Use default_action. Use spaces instead of tabs

--- resources/physical_volume.rb
+++ resources/physical_volume.rb
@@ -1,7 +1,4 @@
-def initialize *args
-    super
-    @action = :create
-end
+actions :create
+default_action :create
 
 attribute :name, :kind_of => String, :name_attribute => true, :required => true
-actions :create
