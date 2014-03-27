f4e49a96cb8e68dd1475d2c41921e43e26f679c1
Douglas Thrift <douglas.thrift@rightscale.com>
2014-03-27 11:29:25 -0700

    [COOK-4486] - Add ChefSpec matchers
    - Add create_lvm_logical_volume, nothing_lvm_logical_volume (since
      logical_volume can be a sub-resource of lvm_volume_group),
      create_lvm_physical_volume, and create_lvm_volume_group ChefSpec
      matchers.

---
+++ libraries/matchers.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: lvm
+# Library:: provider_lvm_volume_group
+#
+# Copyright 2014, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if defined?(ChefSpec)
+  def create_lvm_logical_volume(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:lvm_logical_volume, :create, resource_name)
+  end
+
+  def nothing_lvm_logical_volume(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:lvm_logical_volume, :nothing, resource_name)
+  end
+
+  def create_lvm_physical_volume(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:lvm_physical_volume, :create, resource_name)
+  end
+
+  def create_lvm_volume_group(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:lvm_volume_group, :create, resource_name)
+  end
+end
