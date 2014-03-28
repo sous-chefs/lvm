ec6d75623fb4b52d08600354044d4ed289ce729b
Sean OMeara <someara@opscode.com>
2014-03-27 23:43:17 -0400

    Version bump to 1.0.6

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs lvm2 package'
-version           '1.0.5'
+version           '1.0.6'
 
 supports 'centos'
 supports 'debian'
