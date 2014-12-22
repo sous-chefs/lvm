cf94c63919c1889bbfb2ffe068a96073aa5000f4
Jared Everett <jared.everett@tcnp3.com>
2014-12-22 14:59:22 -0700

    Fix issue with Chef 12.0.x versions

--- libraries/provider_lvm_logical_volume.rb
+++ libraries/provider_lvm_logical_volume.rb
@@ -18,6 +18,7 @@
 #
 
 require 'chef/mixin/shell_out'
+require 'chef/dsl/recipe'
 
 class Chef
   class Provider
@@ -25,6 +26,7 @@ class Chef
     #
     class LvmLogicalVolume < Chef::Provider
       include Chef::Mixin::ShellOut
+      include Chef::DSL::Recipe
 
       # Loads the current resource attributes
       #
