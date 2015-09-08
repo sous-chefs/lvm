7fd064b40bfc42f69968bb9039c33e50a310f926
Tim Smith <tsmith84@gmail.com>
2015-09-08 12:16:10 -0700

    Update URLs and add platforms

--- metadata.rb
+++ metadata.rb
@@ -5,13 +5,11 @@ license 'Apache 2.0'
 description 'Installs and manages Logical Volume Manager'
 version '1.3.7'
 
-supports 'centos'
-supports 'debian'
-supports 'redhat'
-supports 'sles'
-supports 'ubuntu'
+%w(amazon centos fedora freebsd oracle redhat scientific sles ubuntu).each do |os|
+  supports os
+end
 
 recipe 'lvm', 'Installs lvm2 package'
 
-source_url 'https://github.com/opscode-cookbooks/lvm' if respond_to?(:source_url)
-issues_url 'https://github.com/opscode-cookbooks/lvm/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/lvm' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/lvm/issues' if respond_to?(:issues_url)
