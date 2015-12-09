444302de7dd1b9d80d11e64fc82c4a6bd01d255c
Tim Smith <tsmith84@gmail.com>
2015-12-09 10:51:04 -0800

    Use the latest di-ruby-lvm-attrib and di-ruby-lvm gems

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['lvm']['di-ruby-lvm']['version'] = '0.1.3'
-default['lvm']['di-ruby-lvm-attrib']['version'] = '0.0.21'
+default['lvm']['di-ruby-lvm']['version'] = '0.2.1'
+default['lvm']['di-ruby-lvm-attrib']['version'] = '0.0.23'
