1f227e51dbdc5d751def2e023956b7eae1514ee1
Tim Smith <tsmith84@gmail.com>
2016-01-19 20:22:06 -0800

    Update cookbook copyrights

--- libraries/provider_lvm_physical_volume.rb
+++ libraries/provider_lvm_physical_volume.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: lvm
 # Library:: provider_lvm_physical_volume
 #
-# Copyright 2009-2015, Chef Software, Inc.
+# Copyright 2009-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- libraries/resource_lvm_physical_volume.rb
+++ libraries/resource_lvm_physical_volume.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: lvm
 # Library:: resource_lvm_physical_volume
 #
-# Copyright 2009-2013, Chef Software, Inc.
+# Copyright 2009-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
