37f35131cfbbb8a72f9e77ea4c16c20eae84992c
Corey Hemminger <hemminger@hotmail.com>
2016-03-23 13:48:04 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['lvm']['di-ruby-lvm']['version'] = '0.2.1'
 default['lvm']['di-ruby-lvm']['compile_time'] = false
 
-default['lvm']['di-ruby-lvm-attrib']['version'] = '0.0.23'
+default['lvm']['di-ruby-lvm-attrib']['version'] = '0.0.25'
 default['lvm']['di-ruby-lvm-attrib']['compile_time'] = false
