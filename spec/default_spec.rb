1debf66278315e580c7e3d054a7f72927a270acc
Tim Smith <tsmith84@gmail.com>
2016-04-11 09:20:17 -0700

    Remove gem install specs

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -8,12 +8,4 @@ describe 'lvm::default' do
   it 'installs lvm2' do
     expect(chef_run).to install_package('lvm2')
   end
-
-  it 'installs `di-ruby-lvm-attrib` as a Ruby gem' do
-    expect(chef_run).to install_chef_gem('di-ruby-lvm-attrib').with(version: '0.0.25')
-  end
-
-  it 'installs `di-ruby-lvm` as a Ruby gem' do
-    expect(chef_run).to install_chef_gem('di-ruby-lvm').with(version: '0.2.1')
-  end
 end
