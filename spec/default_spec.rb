05c9b4438847df30015470a1243eb5cb1b35dd3c
Tim Smith <tsmith84@gmail.com>
2016-04-11 09:26:40 -0700

    Add spec for the RHEL 7 scenario

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
-describe 'lvm::default' do
+describe 'lvm::default on Ubuntu 14.04' do
   let(:chef_run) do
-    ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '12.04').converge('lvm::default')
+    ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04').converge('lvm::default')
   end
 
   it 'installs lvm2' do
     expect(chef_run).to install_package('lvm2')
+  end
+end
+
+describe 'lvm::default on RHEL 7' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'centos', version: '7.0').converge('lvm::default')
+  end
+
+  before(:each) do
+    stub_command('/sbin/lvm dumpconfig global/use_lvmetad | grep use_lvmetad=1').and_return(true)
+  end
+
+  it 'installs lvm2' do
+    expect(chef_run).to install_package('lvm2')
+  end
+
+  it 'starts / enables lvm2-lvmetad' do
+    expect(chef_run).to start_service('lvm2-lvmetad')
+    expect(chef_run).to enable_service('lvm2-lvmetad')
   end
 end
