0bc127d5c6f0b6a9dfebcab29661a613efdba63d
Tim Smith <tsmith84@gmail.com>
2019-08-27 13:53:55 -0700

    Remove extra metadata from the metadata.rb
    These are unused and not necessary
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,15 +3,11 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and manages Logical Volume Manager'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.5.4'
-
 %w(amazon centos fedora freebsd oracle redhat scientific suse ubuntu).each do |os|
   supports os
 end
 
-recipe 'lvm', 'Installs lvm2 package'
-
 source_url 'https://github.com/chef-cookbooks/lvm'
 issues_url 'https://github.com/chef-cookbooks/lvm/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.1'
