6971e9a8ad787a948a92527f6ce7198fc613cd5f
Tim Smith <tsmith84@gmail.com>
2019-08-27 13:54:41 -0700

    Chef cookstyle fixes in the test recipe
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -6,6 +6,6 @@ description 'A test cookbook for lvm'
 version '0.1.0'
 # The 'test' recipe loop_devices resource requires ruby 2.4.x to operate
 # therefore a minimum chef client needed is 13.0
-chef_version '>= 13.0' if respond_to?(:chef_version)
+chef_version '>= 13.0'
 
 depends 'lvm'
--- test/fixtures/cookbooks/test/recipes/remove.rb
+++ test/fixtures/cookbooks/test/recipes/remove.rb
@@ -22,12 +22,12 @@ apt_update 'update'
 # install the old gems to make sure the uninstall works
 chef_gem 'di-ruby-lvm' do
   compile_time true
-  not_if { '/opt/chef/embedded/bin/gem list | grep "chef-ruby-lvm ""' }
+  not_if '/opt/chef/embedded/bin/gem list | grep "chef-ruby-lvm ""'
 end
 
 chef_gem 'di-ruby-lvm-attrib' do
   compile_time true
-  not_if { '/opt/chef/embedded/bin/gem list | grep chef-ruby-lvm-attrib' }
+  not_if '/opt/chef/embedded/bin/gem list | grep chef-ruby-lvm-attrib'
 end
 
 include_recipe 'lvm'
