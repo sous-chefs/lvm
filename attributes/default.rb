edb527929c4196dc94392c8e742f3c359d8eb1cf
Lance Albertson <lance@osuosl.org>
2021-11-23 07:38:01 -0800

    Add CentOS Stream 8 to CI pipeline (#214)
    * Add CentOS Stream 8 to CI pipeline
    * Update chef-ruby-lvm-attrib gem to 0.3.9
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,5 +18,5 @@
 #
 
 default['lvm']['chef-ruby-lvm']['version'] = '0.4.0'
-default['lvm']['chef-ruby-lvm-attrib']['version'] = '0.3.7'
+default['lvm']['chef-ruby-lvm-attrib']['version'] = '0.3.9'
 default['lvm']['rubysource'] = Chef::Config['rubygems_url']
