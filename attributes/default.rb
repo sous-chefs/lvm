e7d0494797d8eb1572adefde20833573ea355dcc
Roee Fabrikant <50738589+RoeeFabrikant@users.noreply.github.com>
2023-10-24 13:54:47 +0300

    Update chef-ruby-lvm-attrib gem to 0.3.14

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,5 +18,5 @@
 #
 
 default['lvm']['chef-ruby-lvm']['version'] = '0.4.0'
-default['lvm']['chef-ruby-lvm-attrib']['version'] = '0.3.11'
+default['lvm']['chef-ruby-lvm-attrib']['version'] = '0.3.14'
 default['lvm']['rubysource'] = Chef::Config['rubygems_url']
